a7cc72ee3668f89590ea0fb3737c1f928d4a02d5
sarvenaz moayer <127282104+sarvishere@users.noreply.github.com>
2024-07-25 14:11:36 +0330

    final

--- src/components/Form/form.tsx
+++ src/components/Form/form.tsx
@@ -1,12 +1,37 @@
-import React, { useState } from "react";
-import { Button, Modal, Form, Input, Flex, Typography } from "antd";
+import { useState } from "react";
+import {
+  Button,
+  Modal,
+  Form,
+  Input,
+  Flex,
+  Typography,
+  Select,
+  Radio,
+  Divider,
+  RadioChangeEvent,
+  notification,
+} from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 
 import "./form.css"; // Import your custom CSS
 
 const CreateForm = () => {
   const [open, setOpen] = useState(false);
+  const [paymentType, setPaymentType] = useState("account");
   const [form] = Form.useForm();
   const { Text } = Typography;
+
+  const openNotification = () => {
+    notification.success({
+      message: "عملیات موفقیت آمیز بود",
+      description: "تسویه حساب شما ثبت شد",
+      placement: "topRight",
+      style: {
+        direction: "rtl",
+      },
+    });
+  };
 
   const handleCreate = () => {
     form
@@ -14,10 +39,15 @@ const CreateForm = () => {
       .then((values) => {
         form.resetFields();
         setOpen(false);
+        openNotification();
       })
       .catch((info) => {
         console.log("Validate Failed:", info);
       });
+  };
+
+  const handleChangePayment = (e: RadioChangeEvent) => {
+    setPaymentType(e.target.value);
   };
 
   return (
@@ -34,50 +64,107 @@ const CreateForm = () => {
       >
         <div className="custom-modal-header">
           <h2>تسویه کیف پول</h2>
-          <Text type="secondary">اصلی زیب</Text>
+          <Text type="secondary" style={{ fontFamily: "BYekan" }}>
+            اصلی زیب
+          </Text>
         </div>
 
         <Form form={form} layout="vertical">
           <Form.Item>
             <Flex vertical style={{ marginTop: "10px" }}>
               <Text type="secondary"> موجودی فعلی :</Text>
-              <Text>۱۵.۰۰۰ ریال</Text>
+              <Text
+                style={{
+                  fontSize: "20px",
+                  fontFamily: "BYekan",
+                  color: "#1677ff",
+                }}
+              >
+                {" "}
+                ۱۵.۰۰۰ ریال
+              </Text>
             </Flex>
           </Form.Item>
+          <Divider></Divider>
           <Form.Item
-            label="Title"
-            name="title"
+            name="selectOption"
+            rules={[
+              { required: true, message: "لطفا نوع تسویه حساب را وارد کنید" },
+            ]}
+          >
+            <Radio.Group value={paymentType} onChange={handleChangePayment}>
+              <Radio.Button className="radio" value="account">
+                به حساب
+              </Radio.Button>
+              <Radio.Button className="radio" value="wallet">
+                به کیف پول
+              </Radio.Button>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item
+            label="مقصد تسویه"
+            name="destination"
             rules={[
               {
                 required: true,
-                message: "Please input the title of collection!",
+                message: "لطفا مقصد تسویه را انتخاب کنید.",
               },
             ]}
           >
-            <Input />
+            <Select
+              showSearch
+              placeholder=""
+              filterOption={(input, option) =>
+                (option?.label ?? "")
+                  .toLowerCase()
+                  .includes(input.toLowerCase())
+              }
+              options={[
+                { value: "1", label: "۶۲۱۹ ۸۶۱۰ ۱۲۳۴ ۱۲۳۴" },
+                { value: "2", label: "۵۰۲۲ ۲۹۱۰ ۱۲۳۴ ۵۶۷۸" },
+                { value: "3", label: "۶۱۰۳ ۹۹۷۱ ۱۲۳۴ ۵۶۷۸" },
+              ]}
+              style={{
+                direction: "ltr",
+                fontSize: "16px",
+                fontFamily: "BYekan",
+              }}
+            />
           </Form.Item>
 
           <Form.Item
-            label="Title"
-            name="title"
+            label="مبلغ تسویه"
+            name="amount"
             rules={[
               {
                 required: true,
-                message: "Please input the title of collection!",
+                message: "لطفا مبلغ تسویه را وارد کنید.",
               },
             ]}
           >
-            <Input />
+            <Input
+              placeholder="ریال"
+              style={{ direction: "ltr" }}
+              onChange={(e) => {
+                const value = e.target.value.replace(/[^0-9]/g, "");
+                const formattedValue = value.replace(
+                  /\B(?=(\d{3})+(?!\d))/g,
+                  ","
+                );
+                form.setFieldsValue({ amount: formattedValue });
+              }}
+            />
           </Form.Item>
-          <Form.Item name="description" label="Description">
-            <Input type="textarea" />
+          <Form.Item name="description" label="توضیحات (بابت)">
+            <Input.TextArea rows={4} />
           </Form.Item>
         </Form>
 
         <div className="custom-modal-footer">
           <Button onClick={() => setOpen(false)}>انصراف</Button>
           <Button type="primary" onClick={handleCreate}>
-            ثبت درخواست تسویه
+            <span style={{ fontFamily: "BYekan" }}>ثبت تسویه حساب</span>
+            <PlusOutlined></PlusOutlined>
           </Button>
         </div>
       </Modal>
